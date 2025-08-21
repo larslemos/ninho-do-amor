821e1f0324694a033c0b89b9108556de8fa792b3
sblaripas <sblaripas@gmail.com>
2025-08-21 03:17:58 +0200

    Update env variables, fix resend

--- env.ts
+++ env.ts
@@ -11,19 +11,6 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
 });
-
-// export const env = createEnv({
-//   server: {
-//     SUPABASE_SERVICE_ROLE_KEY: z.string().min(1),
-//   },
-//   client: {
-//     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
-//     NEXT_PUBLIC_BASE_URL: z.string().url(),
-//   },
-//   runtimeEnv: {
-//     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-//     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-//   },
-//   emptyStringAsUndefined: true,
-// });
